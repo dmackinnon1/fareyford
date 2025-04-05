3476787ff7b4a9df431a2e5de84ecdcd668d037b
Dan MacKinnon <dmackinnon1@gmail.com>
2025-04-05 14:31:32 -0400

    thickened up

--- js/bldrs.js
+++ js/bldrs.js
@@ -117,7 +117,7 @@ class TikZCircle {
 	}
 
 	build(){
-		let result = "\\draw [ultra thin, color=" + this.color + "] ("+this.x +"," + this.y +") circle (" +this.radius +");\n"; //filldraw
+		let result = "\\draw [thick, color=" + this.color + "] ("+this.x +"," + this.y +") circle (" +this.radius +");\n"; //filldraw
 		return result;
 	}
 };
